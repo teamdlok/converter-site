4bb021ec67e7436ed21f838237001d871011c6db
teamdlok <teamdlok@gmail.com>
2022-09-02 18:54:47 +1000

    Add my project

---
+++ converter_site/static/converter_site/js/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+	var dropZone = $('#upload-container');
+
+	$('#file-input').focus(function() {
+		$('label').addClass('focus');
+	})
+	.focusout(function() {
+		$('label').removeClass('focus');
+	});
+
+
+	dropZone.on('drag dragstart dragend dragover dragenter dragleave drop', function(){
+		return false;
+	});
+
+	dropZone.on('dragover dragenter', function() {
+		dropZone.addClass('dragover');
+	});
+
+	dropZone.on('dragleave', function(e) {
+		let dx = e.pageX - dropZone.offset().left;
+		let dy = e.pageY - dropZone.offset().top;
+		if ((dx < 0) || (dx > dropZone.width()) || (dy < 0) || (dy > dropZone.height())) {
+			dropZone.removeClass('dragover');
+		}
+	});
+
+	dropZone.on('drop', function(e) {
+		dropZone.removeClass('dragover');
+		let files = e.originalEvent.dataTransfer.files;
+		sendFiles(files);
+	});
+
+	$('#file-input').change(function() {
+		let files = this.files;
+		sendFiles(files);
+	});
+
+	 let csrfcookie = function() {
+            let cookieValue = null,
+                name = "csrftoken";
+            if (document.cookie && document.cookie !== "") {
+                let cookies = document.cookie.split(";");
+                for (let i = 0; i < cookies.length; i++) {
+                    let cookie = cookies[i].trim();
+                    if (cookie.substring(0, name.length + 1) == (name + "=")) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        };
+
+	function sendFiles(files) {
+		let maxFileSize = 5242880;
+		let Data = new FormData();
+		$(files).each(function(index, file) {
+			if ((file.size <= maxFileSize) && ((file.type == 'image/png') || (file.type == 'image/jpeg'))) {
+				Data.append('images[]', file);
+			};
+		});
+
+		$.ajax({
+			url: dropZone.attr('action'),
+			type: dropZone.attr('method'),
+			 beforeSend: function(request, settings) {
+				 if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+					 request.setRequestHeader("X-CSRFToken", csrfcookie());
+				 }
+			 },
+			data: Data,
+			contentType: false,
+			processData: false,
+			success: function(data) {
+				alert ('Файлы были успешно загружены!');
+			}
+		});
+	}
+})
